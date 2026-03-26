edf689ffb1ef2b99f4d75397dc83f3c89538bd59
aw <999mashia@gmail.com>
2026-03-27 01:34:41 +0300

    fixing the database column

--- db_sqlalchemy.py
+++ db_sqlalchemy.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine,Column,String, Integer
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine,Column,String, Integer,ForeignKey
+from sqlalchemy.orm import sessionmaker, DeclarativeBase,relationship
 
-engine = create_engine('sqlite:///my_database.db', echo=True)
+engine = create_engine('sqlite:///my_database.db')
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -9,11 +9,65 @@ session = Session()
 class Base(DeclarativeBase):
     pass
 
-class user(Base):
+class User(Base):
     __tablename__ = 'user'
     id = Column(Integer,primary_key=True)
-    name = Column(String(100),nullable=True)
+    name = Column(String(100),nullable=False)
+    message = relationship('Message',back_populates='user')
+
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key = True)
+    content = Column(String(1024),nullable=False)
+    user_id = Column(Integer , ForeignKey('user.id'))
+    user = relationship('User',back_populates='message')
 
 
 
-Base.metadata.create_all(engine)
+def add_user(user):
+     user = User(name = user)
+     session.add(user)
+     session.commit()
+
+
+
+def get_users():
+    users = session.execute(select(User)).scalars().all()
+
+    return users
+
+
+
+
+
+def main():
+
+    while True:
+        print("Welcome to my db")
+        print("1- add user\n2-show users 3- exit")
+        choice = int(input("select a choice:"))
+
+        match choice:
+            case 1:
+                user_name = input("Enter the user name: ")
+                add_user(user_name)
+
+
+
+            case 2:
+                users = get_users()
+                for user in users:
+                    print(user)
+
+
+            case 3:
+
+                break
+
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
+    main()
+
