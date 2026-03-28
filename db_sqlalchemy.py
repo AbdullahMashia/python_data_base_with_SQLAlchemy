6d9de5ff2ff58a9ea68347cb5818b5de0402d7b9
aw <999mashia@gmail.com>
2026-03-28 03:44:13 +0300

    set up ORM classes for all tables

--- db_sqlalchemy.py
+++ db_sqlalchemy.py
@@ -1,73 +1,93 @@
-from sqlalchemy import create_engine,Column,String, Integer,ForeignKey
-from sqlalchemy.orm import sessionmaker, DeclarativeBase,relationship
+from sqlalchemy import create_engine,Integer,String, DateTime,Boolean,select,insert,update,delete,func,ForeignKey
+from sqlalchemy.orm import DeclarativeBase , mapped_column,relationship,sessionmaker
 
-engine = create_engine('sqlite:///my_database.db')
 
+engine = create_engine('sqlite:///yeechatty.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
 
 class Base(DeclarativeBase):
     pass
 
 class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer,primary_key=True)
-    name = Column(String(100),nullable=False)
-    message = relationship('Message',back_populates='user')
+    __tablename__ = 'users'
+
+    id = mapped_column(Integer, primary_key = True)
+    username = mapped_column(String(50),nullable= False,unique=True)
+    password = mapped_column(String(100), nullable=False)
+    email = mapped_column(String(100),nullable=False)
+    country = mapped_column(String(30),nullable=False)
+    age = mapped_column(Integer,nullable=False)
+    created = mapped_column(DateTime,server_default = func.now,nullable=False)
+
+    # relations
 
 
-class Message(Base):
-    __tablename__ = 'message'
-    id = Column(Integer, primary_key = True)
-    content = Column(String(1024),nullable=False)
-    user_id = Column(Integer , ForeignKey('user.id'))
-    user = relationship('User',back_populates='message')
+    r_reqMsg_receiver = relationship('Request_messaging',back_populates= 'r_receiver')
+    r_reqMsg_sender = relationship('Request_messaging', back_populates = 'r_sender')
+    r_Msgs = relationship('Messages', back_populates = 'r_User')
+
+    r_CP = relationship('Conversations_participants',back_populates = 'r_User')
 
 
-
-def add_user(user):
-     user = User(name = user)
-     session.add(user)
-     session.commit()
-
-
-
-def get_users():
-    users = session.execute(select(User)).scalars().all()
-
-    return users
+class Conversations(Base):
+    __tablename__ = 'convs'
+    id = mapped_column(Integer, primary_key = True)
+    created_at = mapped_column(DateTime , server_default = func.now, nullable = False)
+    enc_key = mapped_column(String(512),nullable=False)
 
 
+    # relations
+
+    r_CP  = relationship('Conversations_participants', back_populates = 'r_C')
+    r_Msgs = relationship('Messages', back_populates = 'r_C')
 
 
-
-def main():
-
-    while True:
-        print("Welcome to my db")
-        print("1- add user\n2-show users 3- exit")
-        choice = int(input("select a choice:"))
-
-        match choice:
-            case 1:
-                user_name = input("Enter the user name: ")
-                add_user(user_name)
+class Conversations_participants(Base):
+    __tablename__ = 'conv_mems'
+    id = mapped_column(Integer, primary_key =True)
+    conv_id = mapped_column(Integer , ForeignKey('convs.id'))
+    user_id = mapped_column(Integer,ForeignKey('users.id'))
 
 
+    # relations
+    r_C = relationship('Conversations',back_populates = 'r_CP')
 
-            case 2:
-                users = get_users()
-                for user in users:
-                    print(user)
-
-
-            case 3:
-
-                break
+    r_User = relationship('User', back_populates ='r_CP')
 
 
 
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
-    main()
+class Request_messaging(Base):
+    __tablename__ = 'req_msg'
+    id = mapped_column(Integer, primary_key = True)
+    sender_id = mapped_column(Integer, ForeignKey('users.id'))
+    receiver_id = mapped_column(Integer,ForeignKey('users.id'))
+    request_state = mapped_column(String(10),server_default = 'pending')
+    created_at = mapped_column(DateTime, server_default = func.now)
+
+
+    # relations:
+    r_receiver = relationship('User',back_populates = 'r_reqMsg_receiver',foreign_keys = [receiver_id])
+    r_sender = relationship('User', back_populates = 'r_reqMsg_sender', foreign_keys= [sender_id])
+
+
+
+class Messages(Base):
+    __tablename__ = 'msgs'
+    id = mapped_column(Integer , primary_key=  True)
+    conv_id = mapped_column(Integer, ForeignKey('convs.id'))
+    sender_id = mapped_column(Integer, ForeignKey('users.id'))
+    content = mapped_column(String(1024), nullable=False)
+    sent_at = mapped_column(DateTime, server_default = func.now)
+    type = mapped_column(String(10),server_default = 'text')
+
+
+    #relations
+    r_C = relationship('Conversations',back_populates= 'r_Msgs')
+    r_User = relationship('User', back_populates = 'r_Msgs')
+
+
+
 
